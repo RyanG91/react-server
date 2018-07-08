4efd39306ab614199a8d2dd340f7503dbb1ac692
Ryan Grosser <33079422+RyanG91@users.noreply.github.com>
2018-07-08 20:51:42 +1000

    Update app.js

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const cors = require('cors')
 
+
 const { initializePassport, requireJwt } = require('./middleware/auth')
 
 const app = express();
